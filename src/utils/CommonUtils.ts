38d4170efd125488fc2e0a464d82bd9fed605301
maximum0 <maximum.zero95@gmail.com>
2025-07-16 15:02:55 +0900

    fix: request datetime timezone 설정

--- src/utils/CommonUtils.ts
+++ src/utils/CommonUtils.ts
@@ -1,20 +1,19 @@
+import { formatInTimeZone } from "date-fns-tz";
+
 export default class CommonUtils {
   /**
-   * Date 객체를 'YYMMDDHHmmss' 형식의 문자열로 포맷팅합니다.
+   * Date 객체를 'yyMMddHHmmss' 형식의 문자열로 포맷팅합니다.
    * @param date 포맷팅할 Date 객체
    * @returns 포맷팅된 날짜/시간 문자열
    */
-  static formatDateTime(date: Date | null): string {
-    if (!date) {
-      return "";
+  static formatDateTime(
+    dateValue: Date | null | undefined,
+    formatString: string = "yyMMddHHmmss",
+    timeZone: string = "Asia/Seoul"
+  ): string | undefined {
+    if (dateValue instanceof Date) {
+      return formatInTimeZone(dateValue, timeZone, formatString);
     }
-
-    const year = date.getFullYear().toString().slice(-2);
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+    return undefined;
   }
 }
